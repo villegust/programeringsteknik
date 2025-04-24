41cf7230763a182b9366a64cc7c5d0571efb7863
Vilhelm <ville.gust@gmail.com>
2025-04-24 10:35:51 +0200

    stuff

--- inlämningar/uppgift3.py
+++ inlämningar/uppgift3.py
@@ -16,13 +16,11 @@ def word_count(t):
 def part2(element):
     return element[1]
 
-with open("dikt.txt", "r", encoding='utf8') as text_file:
+with open("dikt.txt", "r", encoding="utf8") as text_file:
     text = text_file.read().lower()
 
 #Tar bort alla specialtecken och delar upp det i en lista med alla ord
 wordlist = re.findall(r"[a-zA-ZåäöÅÄÖ]+", text)
-
-#Splitar upp texten och ränkar alla antal ord i "split_text" 
 
 common_word= int(input("Hur många av de vanligaste orden vill du se?: "))
 rare_word= int(input("Hur många av de ovanligaste orden vill du se?: "))
@@ -31,15 +29,9 @@ rare_word= int(input("Hur många av de ovanligaste orden vill du se?: "))
 print(f"Totalt antal ord: {len(wordlist)}")
 print(f"Totalt antal unika ord: {len(set(wordlist))} \n")
 
-"""
-sorted_words konverterar word_count(wordlist) till en lista med key value pairs.
-lambda x: x[1] sorterar efter den andra elementet, vilket är antalet.
-
-""" 
 sorted_words = sorted(word_count(wordlist).items(), key=part2, reverse=True)
 
 #Loopar igenom de valigaste orden och printar dem enligt antalet man har anget i common_word
-
 print(f"De {common_word} vanligaste orden var (antal förekommer inom parantes): ")
 for i in range(0, common_word):
     word, count = sorted_words[i]
@@ -52,4 +44,3 @@ print(f"De {rare_word} ovanligaste orden var (antal förekommer inom parantes): 
 for i in range(0, rare_word):
     word, count = sorted_words[-(i + 1)]
     print(f"{i + 1}. {word:<20} ({count})")
-
