1e24d79694619007d9ea205c3bd6c7ca319b9a32
Vilhelm <ville.gust@gmail.com>
2025-04-24 10:19:29 +0200

    Små ändringar till uppgift 3

--- inlämningar/uppgift3.py
+++ inlämningar/uppgift3.py
@@ -13,9 +13,8 @@ def word_count(t):
 
     return count
 
-filename = "dikt.txt"
 
-with open(filename, "r", encoding='utf8') as text_file:
+with open("dikt.txt", "r", encoding='utf8') as text_file:
     text = text_file.read().lower()
 
 #Tar bort alla specialtecken och delar upp det i en lista med alla ord
@@ -38,24 +37,17 @@ lambda x: x[1] sorterar efter den andra elementet, vilket är antalet.
 sorted_words = sorted(word_count(wordlist).items(), key=lambda x: x[1], reverse=True)
 
 #Loopar igenom de valigaste orden och printar dem enligt antalet man har anget i common_word
-if common_word > len(set(wordlist)) or common_word < 0:
-    print(f"{common_word} vanliga ord finns inte i texten. Max antal är {len(set(wordlist))}")
 
-else:
-    print(f"De {common_word} vanligaste orden var (antal förekommer inom parantes): ")
-    for i in range(0, common_word):
-        word, count = sorted_words[i]
-        print(f"{i + 1}. {word:<20} ({count})")
+print(f"De {common_word} vanligaste orden var (antal förekommer inom parantes): ")
+for i in range(0, common_word):
+    word, count = sorted_words[i]
+    print(f"{i + 1}. {word:<20} ({count})")
 
 print("\n")
 
 #Loopar igenom de ovaligaste orden och printar dem enligt antalet man har anget i rare_word
-if rare_word > len(set(wordlist)) or rare_word < 0:
-    print(f"{rare_word} ovanliga ord finns inte i texten. Max antal är {len(set(wordlist))}")
-
-else:
-    print(f"De {rare_word} ovanligaste orden var (antal förekommer inom parantes): ")
-    for i in range(0, rare_word):
-        word, count = sorted_words[-(i + 1)]
-        print(f"{i + 1}. {word:<20} ({count})")
+print(f"De {rare_word} ovanligaste orden var (antal förekommer inom parantes): ")
+for i in range(0, rare_word):
+    word, count = sorted_words[-(i + 1)]
+    print(f"{i + 1}. {word:<20} ({count})")
 
